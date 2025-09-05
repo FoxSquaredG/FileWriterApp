10e52ef606f951f8c9aa758a12fb89725ab4b3f1
foxsquaredm <foxsquaredm@gmail.com>
2025-09-05 13:51:08 +0300

    read01

--- FileWriterApp/resource.h
+++ FileWriterApp/resource.h
@@ -10,8 +10,11 @@
 #define IDC_EDIT_FILEPATH               1000
 #define IDC_EDIT_TEXT                   1001
 #define IDC_BUTTON_WRITE                1002
-#define IDC_BUTTON2                     1003
 #define IDC_BUTTON_BROWSE               1003
+#define IDC_TAB1                        1004
+#define IDC_BUTTON_READ                 1005
+#define IDC_EDIT1                       1006
+#define IDC_EDIT_CONTENT                1006
 
 // Next default values for new objects
 // 
@@ -19,7 +22,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        130
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1004
+#define _APS_NEXT_CONTROL_VALUE         1007
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
